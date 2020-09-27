447c6023ca3c3a56fc1f37f166bbc722f1eb7537
Tim Reddish <xynomix@gmail.com>
2020-09-27 18:25:04 +0100

    Stage 3: Check the levels of the coffee machine

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Coffee Machine project from JetBrains Academy 
 - Stage 1: Text output of the Coffee Making process
 - Stage 2: Calculate the amount of ingredients for a set number of cups
+- Stage 3: Check the levels of the coffee machine
--- coffee_machine.py
+++ coffee_machine.py
@@ -10,12 +10,26 @@ def get_beans(cups):
     return 15 * cups
 
 
-def make_coffee():
-    cups_required = int(input("Write how many cups of coffee you will need: "))
-    print(f"For {cups_required} cups of coffee you will need:")
-    print(f"{get_water(cups_required)} ml of water")
-    print(f"{get_milk(cups_required)} ml of milk")
-    print(f"{get_beans(cups_required)} g of coffee beans")
+class CoffeeMachine:
+    def __init__(self):
+        self.water = int(input("Write how many ml of water the coffee machine has: "))
+        self.milk = int(input("Write how many ml of milk the coffee machine has: "))
+        self.beans = int(input("Write how many grams of coffee beans the coffee machine has: "))
+
+    def get_stock(self):
+        return min(self.water // 200, self.milk // 50, self.beans // 15)
+
+    def make_coffee(self, cups):
+        stock = self.get_stock()
+        if stock == cups:
+            print("Yes, I can make that amount of coffee")
+        elif stock > cups:
+            print(f"Yes, I can make that amount of coffee (and even {stock - cups} more than that)")
+        else:
+            print(f"No, I can make only {self.get_stock()} cups of coffee")
 
 
-make_coffee()
+my_coffee_machine = CoffeeMachine()
+
+cups_required = int(input("Write how many cups of coffee you will need: "))
+my_coffee_machine.make_coffee(cups_required)
