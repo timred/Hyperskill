44d2ecadb83305dd2c1c0738a64ad7a1c6a0f1fb
Tim Reddish <xynomix@gmail.com>
2020-10-06 19:26:00 +0100

    Stage 3: Adding a SQLite3 database

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Simple Banking System from JetBrains Academy
 - Stage 1: Anatomy of a bank card
 - Stage 2: Luhn Algorithm
-
+- Stage 3: Add SQLite database
--- banking.py
+++ banking.py
@@ -1,4 +1,19 @@
 import random
+import sqlite3
+
+
+def create_database():
+    conn = sqlite3.connect("card.s3db")
+    cur = conn.cursor()
+    try:
+        cur.execute("drop table card")
+        conn.commit()
+    except sqlite3.OperationalError:
+        pass
+
+    cur.execute("create table if not exists card (id integer, number text, pin text, balance integer default 0)")
+    conn.commit()
+    conn.close()
 
 
 class Bank:
@@ -6,21 +21,41 @@ class Bank:
 
     def __init__(self):
         self.bank_id_number = '400000'
-        self.account_id = self.new_account_id()
+        self.account_id = new_account_id()
         self.checksum = str(luhn(self.bank_id_number + self.account_id, validate=False))
         self.card_number = self.bank_id_number + self.account_id + self.checksum
         self.pin = new_pin()
         self.balance = 0
-        self.cards[self.card_number] = self
+        set_card(self)
 
-    def new_account_id(self):
-        while True:
-            account_id = str(random.randint(0, 999999999))
-            account_id = account_id.zfill(9)
-            for card_number in self.cards.keys():
-                if self.cards[card_number].account_id == account_id:
-                    continue
-            return account_id
+
+def new_account_id():
+    while True:
+        account_id = str(random.randint(0, 999999999))
+        account_id = account_id.zfill(9)
+        if get_card(account_id):
+            continue
+        return account_id
+
+
+def set_card(card):
+    query = f"insert into card (id, number, pin, balance) " \
+            f"values ({int(card.account_id)}, '{card.card_number}', '{card.pin}', {card.balance})"
+    conn = sqlite3.connect("card.s3db")
+    c = conn.cursor()
+    c.execute(query)
+    conn.commit()
+    conn.close()
+
+
+def get_card(search):
+    query = f"select number, pin, balance from card where number = {search} or id = {search}"
+    conn = sqlite3.connect("card.s3db")
+    c = conn.cursor()
+    c.execute(query)
+    card = c.fetchone()
+    conn.close()
+    return card
 
 
 def luhn(number, validate=True):
@@ -46,7 +81,8 @@ def new_pin():
 
 
 def authenticate(card_number, pin_number):
-    if card_number in Bank.cards and Bank.cards[card_number].pin == pin_number:
+    card = get_card(card_number)
+    if card and card_number == card[0] and pin_number == card[1]:
         return True
     else:
         return False
@@ -79,12 +115,12 @@ def main():
             pin_number = input("Enter you PIN:\n")
             if authenticate(card_number, pin_number):
                 print("You have successfully logged in!")
-                card = Bank.cards[card_number]
+                card = get_card(card_number)
                 while True:
                     menu(1)
                     choice = int(input())
                     if choice == 1:
-                        print(f"Balance: {card.balance}")
+                        print(f"Balance: {card[2]}")
                     elif choice == 2:
                         print("You have successfully logged out!")
                         break
@@ -99,4 +135,5 @@ def main():
     print("Bye!")
 
 
+create_database()
 main()
