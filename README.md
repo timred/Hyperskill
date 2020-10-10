78361ffbadcd716bf0a943318311ee1e6a0db342
Tim Reddish <xynomix@gmail.com>
2020-10-10 21:27:45 +0100

    Stage 4: Use the requests library to GET pages

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Text-Based Browser from JetBrains Academy
 - Stage 1: Address line
 - Stage 2: Get pages with / without cache
 - Stage 3: Implement a Back button
+- Stage 4: Use requests library
--- browser.py
+++ browser.py
@@ -1,43 +1,18 @@
 import os
 import sys
 import re
+import requests
 from collections import deque
 
 
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
+def get_page(url):
+    if not re.match(r"^http[s]://", url):
+        url = "https://" + url
 
-Scientists have created “soft” magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
+    response = requests.get(url)
+    return response.text
 
 
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-'''
 history = deque()
 
 if __name__ == "__main__":
@@ -70,9 +45,11 @@ if __name__ == "__main__":
         file_path = os.path.join(save_dir, site.split(".", 1)[0])
         if re.match(r"^.+\.", site):
             try:
-                page = globals()[site.replace(".", "_")]
+                # TODO: Request Page
+                # page = globals()[site.replace(".", "_")]
+                page = get_page(site)
                 print(page)
-                with open(file_path, "w") as file:
+                with open(file_path, "w", encoding="utf-8") as file:
                     file.write(page)
             except KeyError:
                 print("Error: Page not found")
