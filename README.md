1ff5710bdd27b045aaf8e75515aa2b94c8c2fa3c
Tim Reddish <xynomix@gmail.com>
2021-02-24 01:09:51 +0000

    Stage 4: Special stops

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Easy Rider Bus Company from JetBrains Academy
 - Stage 1: Checking the data type
 - Stage 2: Correct Syntax
 - Stage 3: Bus line info
+- Stage 4: Special Stops
--- easyrider.py
+++ easyrider.py
@@ -75,14 +75,59 @@ def invalid_buses(buses):
     return errors
 
 
-def parse_stops(buses):
+def invalid_route(buses):
     bus_stops = dict()
     for bus in buses:
         try:
-            bus_stops[bus["bus_id"]].add(bus["stop_id"])
+            bus_stops[bus["bus_id"]].append(bus["stop_type"])
         except KeyError:
-            bus_stops[bus["bus_id"]] = {bus["stop_id"]}
+            bus_stops[bus["bus_id"]] = [bus["stop_type"]]
+
+    for bus, stops_list in bus_stops.items():
+        if stops_list.count("S") == 1 and stops_list.count("F") == 1:
+            continue
+        else:
+            return bus
+    return False
+
+
+def parse_stop_types(buses, bus_stops):
+    stop_types = dict()
+    for bus in buses:
+        try:
+            stop_types[bus["stop_type"]].add(bus["stop_id"])
+        except KeyError:
+            stop_types[bus["stop_type"]] = {bus["stop_id"]}
+
+    bus_lines = parse_lines(buses)
+    for stop in bus_stops.keys():
+        counter = 0
+        for line in bus_lines.values():
+            if stop in line:
+                counter += 1
+        if counter >= 2:
+            try:
+                stop_types["T"].add(stop)
+            except KeyError:
+                stop_types["T"] = {stop}
+    return stop_types
+
+
+def stops(buses):
+    bus_stops = dict()
+    for bus in buses:
+        bus_stops[bus["stop_id"]] = bus["stop_name"]
     return bus_stops
+
+
+def parse_lines(buses):
+    bus_lines = dict()
+    for bus in buses:
+        try:
+            bus_lines[bus["bus_id"]].add(bus["stop_id"])
+        except KeyError:
+            bus_lines[bus["bus_id"]] = {bus["stop_id"]}
+    return bus_lines
 
 
 def print_type_required_validation(invalid_data):
@@ -102,11 +147,23 @@ def print_format_validation(invalid_data):
 
 def print_bus_stops(bus_stops):
     print("Line names and number of stops:")
-    for bus, stops in bus_stops.items():
-        print(f"bus_id: {bus}, stops: {len(stops)}")
+    for bus, stops_list in bus_stops.items():
+        print(f"bus_id: {bus}, stops: {len(stops_list)}")
+
+
+def print_stop_types(stop_types, bus_stops):
+    print(f"Start stops: {len(stop_types['S'])} {sorted([bus_stops[stop] for stop in stop_types['S']])}")
+    print(f"Transfer stops: {len(stop_types['T'])} {sorted([bus_stops[stop] for stop in stop_types['T']])}")
+    print(f"Finish stops: {len(stop_types['F'])} {sorted([bus_stops[stop] for stop in stop_types['F']])}")
 
 
 if __name__ == "__main__":
     easy_rider = json.loads(input())
-    easy_rider_stops = parse_stops(easy_rider)
-    print_bus_stops(easy_rider_stops)
+    invalid = invalid_route(easy_rider)
+    if invalid:
+        print(f"There is no start or end stop for the line: {invalid}.")
+    else:
+        easy_rider_stops = stops(easy_rider)
+        easy_rider_types = parse_stop_types(easy_rider, easy_rider_stops)
+        print_stop_types(easy_rider_types, easy_rider_stops)
+
