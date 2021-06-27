4490e21d654e7688bb8117987a7471ad5b30d193
Tim Reddish <xynomix@gmail.com>
2021-06-27 16:30:44 +0100

    Piece Positions.

--- game.py
+++ game.py
@@ -40,64 +40,75 @@ class Piece:
 
 class O(Piece):
 
-    def __init__(self, board_width, debug=False):
+    def __init__(self, board, debug=False):
         # positions = [[4, 14, 15, 5]]
-        positions = [[(board_width - 1) // 2 + board_width * i for i in range(2)]
-                     + [(board_width - 1) // 2 + board_width * i + 1 for i in range(2)]]
+        positions = [[board.mid_point + board.width * i for i in range(2)]
+                     + [board.mid_point + board.width * i + 1 for i in range(2)]]
         super().__init__(positions, debug)
 
 
 class I(Piece):
 
-    def __init__(self, board_width, debug=False):
+    def __init__(self, board, debug=False):
         # positions = [[4, 14, 24, 34], [3, 4, 5, 6]]
-        positions = [[(board_width - 1) // 2 + board_width * i for i in range(4)],
-                     [board_width // 2 - 2 + i for i in range(4)]]
+        positions = [[board.mid_point + board.width * i for i in range(4)],
+                     [board.mid_point - 1 + i for i in range(4)]]
         super().__init__(positions, debug)
 
 
 class S(Piece):
 
-    def __init__(self, board_width, debug=False):
+    def __init__(self, board, debug=False):
         # positions = [[5, 4, 14, 13], [4, 14, 15, 25]]
-        positions = [[(board_width - 1) // 2 + i for i in range(2)]
-                     + [(board_width - 1) // 2 + board_width - i for i in range(2)],
-                     [(board_width - 1) // 2 + board_width * i for i in range(2)]
-                     + [(board_width - 1) // 2 + 1 + board_width * (i + 1) for i in range(2)]]
+        positions = [[board.mid_point + i for i in range(2)]
+                     + [board.mid_point + board.width - i for i in range(2)],
+                     [board.mid_point + board.width * i for i in range(2)]
+                     + [board.mid_point + 1 + board.width * (i + 1) for i in range(2)]]
         super().__init__(positions, debug)
 
 
 class Z(Piece):
 
-    def __init__(self, board_width, debug=False):
+    def __init__(self, board, debug=False):
         # positions = [[4, 5, 15, 16], [5, 15, 14, 24]]
-        positions = [[(board_width - 1) // 2 + i for i in range(2)]
-                     + [(board_width - 1) // 2 + 1 + board_width + i for i in range(2)],
-                     [(board_width - 1) // 2 + 1 + board_width * i for i in range(2)]
-                     + [(board_width - 1) // 2 + board_width * (i + 1) for i in range(2)]]
+        positions = [[board.mid_point + i for i in range(2)]
+                     + [board.mid_point + 1 + board.width + i for i in range(2)],
+                     [board.mid_point + 1 + board.width * i for i in range(2)]
+                     + [board.mid_point + board.width * (i + 1) for i in range(2)]]
         super().__init__(positions, debug)
 
 
 class L(Piece):
 
-    def __init__(self, board_width, debug=False):
+    def __init__(self, board, debug=False):
         # positions = [[4, 14, 24, 25], [5, 15, 14, 13], [4, 5, 15, 25], [6, 5, 4, 14]]
-        positions = [[(board_width - 1) // 2 + board_width * i for i in range(3)] + [(board_width - 1) // 2 + board_width * 2 + 1]]
+        positions = [[board.mid_point + board.width * i for i in range(3)] + [board.mid_point + board.width * 2 + 1],
+                     [board.mid_point - 1 + board.width + i for i in range(3)] + [board.mid_point + 1],
+                     [board.mid_point + i for i in range(2)] + [(board.width - 1) // 2 + 1 + board.width * (i + 1) for i in range(2)],
+                     [board.mid_point + i for i in range(3)] + [(board.width - 1) // 2 + board.width]]
         super().__init__(positions, debug)
 
 
 class J(Piece):
 
-    def __init__(self, board_width, debug=False):
-        positions = [[5, 15, 25, 24], [15, 5, 4, 3], [5, 4, 14, 24], [4, 14, 15, 16]]
+    def __init__(self, board, debug=False):
+        # positions = [[5, 15, 25, 24], [15, 5, 4, 3], [5, 4, 14, 24], [4, 14, 15, 16]]
+        positions = [[board.mid_point + 1 + board.width * i for i in range(3)] + [board.mid_point + board.width * 2],
+                     [board.mid_point - 1 + i for i in range(3)] + [board.mid_point + 1 + board.width],
+                     [board.mid_point + 1] + [board.mid_point + board.width * i for i in range(3)],
+                     [board.mid_point] + [board.mid_point + board.width + i for i in range(3)]]
         super().__init__(positions, debug)
 
 
 class T(Piece):
 
-    def __init__(self, board_width):
-        positions = [[4, 14, 24, 15], [4, 13, 14, 15], [5, 15, 25, 14], [4, 5, 6, 15]]
-        super().__init__(positions)
+    def __init__(self, board, debug=False):
+        # positions = [[4, 14, 24, 15], [4, 13, 14, 15], [5, 15, 25, 14], [4, 5, 6, 15]]
+        positions = [[board.mid_point + board.width * i for i in range(3)] + [board.mid_point + 1 + board.width],
+                     [board.mid_point] + [board.mid_point - 1 + board.width + i for i in range(3)],
+                     [board.mid_point + 1 + board.width * i for i in range(3)] + [board.mid_point + board.width],
+                     [board.mid_point + i for i in range(3)] + [board.mid_point + 1 + board.width]]
+        super().__init__(positions, debug)
 
 
 class Board:
@@ -107,6 +118,7 @@ class Board:
             height = width
         self.width = width
         self.height = height
+        self.mid_point = (self.width - 1) // 2
         self.board = np.full((self.height, self.width), "-")
         self.pieces = []
 
@@ -124,43 +136,25 @@ class Board:
 
 
 def main():
-    piece_i = Piece([[1, 5, 9, 13], [4, 5, 6, 7], [1, 5, 9, 13], [4, 5, 6, 7]])
-    piece_s = Piece([[6, 5, 9, 8], [5, 9, 10, 14], [6, 5, 9, 8], [5, 9, 10, 14]])
-    piece_z = Piece([[4, 5, 9, 10], [2, 5, 6, 9], [4, 5, 9, 10], [2, 5, 6, 9]])
-    piece_l = Piece([[1, 5, 9, 10], [4, 5, 6, 2], [0, 1, 5, 9], [4, 5, 6, 8]])
-    piece_j = Piece([[2, 6, 9, 10], [4, 5, 6, 10], [1, 2, 5, 9], [0, 4, 5, 6]])
-    piece_o = Piece([[5, 6, 9, 10], [5, 6, 9, 10], [5, 6, 9, 10], [5, 6, 9, 10]])
-    piece_t = Piece([[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]])
-
-    pieces = {
-        "I": piece_i,
-        "S": piece_s,
-        "Z": piece_z,
-        "L": piece_l,
-        "J": piece_j,
-        "O": piece_o,
-        "T": piece_t
-    }
-
-    user_input = input()
-    print()
-    piece = pieces.get(user_input)
-    piece.print_positions()
-    piece.print_positions(start=True)
+    pass
 
 
 if __name__ == "__main__":
     # main()
-    board = Board(10, 20)
-    print(board)
-    i_piece = I(board.width)
-    o_piece = O(board.width)
-    s_piece = S(board.width)
-    z_piece = Z(board.width)
-    l_piece = L(board.width, debug=True)
-    # board.add_piece(i_piece)
-    # board.add_piece(o_piece)
-    # board.add_piece(s_piece)
-    # board.add_piece(z_piece)
-    board.add_piece(l_piece)
-    print(board)
+    tetris_board = Board(10, 20)
+    print(tetris_board)
+    i_piece = I(tetris_board)
+    o_piece = O(tetris_board)
+    s_piece = S(tetris_board)
+    z_piece = Z(tetris_board)
+    l_piece = L(tetris_board)
+    j_piece = J(tetris_board)
+    t_piece = T(tetris_board, debug=True)
+    # tetris_board.add_piece(i_piece)
+    # tetris_board.add_piece(o_piece)
+    # tetris_board.add_piece(s_piece)
+    # tetris_board.add_piece(z_piece)
+    # tetris_board.add_piece(l_piece)
+    # tetris_board.add_piece(j_piece)
+    tetris_board.add_piece(t_piece)
+    print(tetris_board)
