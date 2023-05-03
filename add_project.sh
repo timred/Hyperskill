e35eaae38ad134ff41e8cc61c174aab8a8003667
Tim Reddish <xynomix@gmail.com>
2023-05-03 19:17:31 +0100

    Add: CoffeeMachine

---
+++ add_project.sh
@@ -0,0 +1,6 @@
+# Merge Project-A into Project-B
+cd path/to/project-b
+git remote add project-a /path/to/project-a
+git fetch project-a --tags
+git merge --allow-unrelated-histories project-a/master # or whichever branch you want to merge
+git remote remove project-a
